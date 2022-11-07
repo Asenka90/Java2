d8b397b24c7e71aa74c70a5bb10670ecc0c7363f
Анастасия <89359100+Asenka90@users.noreply.github.com>
2022-11-07 20:34:46 +0400

    Add files via upload

---
+++ T3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class T3 {
+public static void main(String[] args) {
+    String targetStr = ".";
+    String tempStr="";
+    String currPathname = System.getProperty("user.dir");
+    File currDir = new File (currPathname);
+    File[] files = currDir.listFiles();
+    String [] extensions = new String[files.length];
+    System.out.println("В текущей папке существуют файлы со следующими расширениями: ");
+    for(int i=0; i<files.length; i++){
+       tempStr=files[i].toString();
+       extensions[i]=tempStr.substring(tempStr.indexOf(targetStr));
+       System.out.println(i+1 +". "+ "Расширение файла:"+ "'"+extensions[i]+"'");
+    }
+}
+}
