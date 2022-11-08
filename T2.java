c16028030ad5140f978e0ad13003c7b0785bac17
Анастасия <89359100+Asenka90@users.noreply.github.com>
2022-11-08 13:07:45 +0400

    Add files via upload

--- T2.java
+++ T2.java
@@ -1,24 +1,41 @@
 import java.util.Arrays;
- 
-public class T2 {
+import java.util.logging.*;
+
+public class T2{
     public static void main(String[] args) {
-        int [] mas = {11, 3, 14, 16, 7};
- 
-        boolean isSorted = false;
-        int buf;
-        while(!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < mas.length-1; i++) {
-                if(mas[i] > mas[i+1]){
-                    isSorted = false;
- 
-                    buf = mas[i];
-                    mas[i] = mas[i+1];
-                    mas[i+1] = buf;
-                    
+    var array = new int[]{10, 8, 6, 12, 11, 6, 1, 3, 5};
+    System.out.println("Изначальный массив: " + Arrays.toString(array));
+    Sort(array);
+    System.out.println("Новый массив: " + Arrays.toString(array));
+
+    }
+
+   
+
+    private static void Sort(int[] array) {
+        
+
+        var sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (var i = 1; i < array.length; i++) {
+                var previous = array[i - 1];
+                var current = array[i];
+                logger.info("Элемент итерации равен " + current);
+                if (previous > current) {
+                    swap(array, i - 1, i);
+                    sorted = false;
                 }
             }
         }
-        System.out.println(Arrays.toString(mas));
     }
+
+    private static void swap(int[] array, int index1, int index2) {
+        var buffer = array[index1];
+        array[index1] = array[index2];
+        array[index2] = buffer;
+    }
+
+    public static final Logger logger = Logger.getLogger("main");
+   
 }
